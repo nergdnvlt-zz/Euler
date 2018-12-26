8b6597bea8907c65bf57bb7ef642db338d7affb0
Tyler Lundgren <tylerlundgren@yahoo.com>
2018-12-26 15:55:35 -0700

    TDD for summing multiple arrays.

--- 1_ruby/test/test_project.rb
+++ 1_ruby/test/test_project.rb
@@ -5,4 +5,17 @@ require 'pry'
 require './lib/project'
 
 class ProjectTest< MiniTest::Test
+  def test_it_exists_and_takes_args
+    project = Project.new(10, 3, 5)
+
+    assert_instance_of Project, project
+  end
+
+  def test_finds_sum_of_all_multiples
+    project = Project.new(10, 3, 5)
+
+    projected = 23
+    result = project.sum
+    assert_equal result, projected
+  end
 end
