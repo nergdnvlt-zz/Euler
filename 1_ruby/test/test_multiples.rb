854c92e00fde5c78da14b1a4a8d63cc60ae0ca21
Tyler Lundgren <tylerlundgren@yahoo.com>
2018-12-26 15:27:38 -0700

    TDD for building out multiples.

--- 1_ruby/test/test_multiples.rb
+++ 1_ruby/test/test_multiples.rb
@@ -5,4 +5,16 @@ require 'pry'
 require './lib/multiples'
 
 class MultiplesTest< MiniTest::Test
+  def test_it_exists
+    multiple = Multiple.new()
+
+    assert_instance_of Multiple, multiple
+  end
+
+  def test_finds_multiple_of_three
+    multiples = Multiple.new(3, 10)
+
+    projected = [3, 6, 9]
+    assert_equal multiples, projected
+  end
 end
