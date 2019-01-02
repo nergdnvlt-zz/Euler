1d2e2fdfc670756b3dc5c67c62696b599e75c8fb
Tyler Lundgren <tylerlundgren@yahoo.com>
2019-01-02 09:33:09 -0700

    Adding tdd for summing evens.

--- 2_ruby/test/test_sum.rb
+++ 2_ruby/test/test_sum.rb
@@ -3,8 +3,20 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require 'pry'
 
-require './lib/sum'
+require './lib/sum_evens'
 
 # Test for the fibonacci class.
-class SumTest < MiniTest::Test
+class SumEvenTest < MiniTest::Test
+  def test_it_exists
+    sum = SumEvens.new(10)
+    assert_instance_of SumEvens, sum
+  end
+
+  def test_returns_sequence
+    sum = SumEvens.new(10)
+    result = sum.result
+    expected = [1, 2, 3, 5, 8]
+
+    assert_equal expected, result
+  end
 end
